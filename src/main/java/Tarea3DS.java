cf3c5f5fd9c203676e9271f92caff970f8fa4eee
Maycol Mont <maycol.montalvan1405@gmail.com>
2025-08-10 10:13:34 -0500

    firstCommit

---
+++ src/main/java/Tarea3DS.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author maycmnont
+ */
+public class Tarea3DS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
